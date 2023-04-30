30da35f8adeb642f22b6f57959f773989b943a85
ilya <shakurov.ia@gmail.com>
2023-04-30 16:23:39 +0300

    feat: add material ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
 
 export const Header = () => (
-  <header>
+  <Box
+    sx={{
+      display: 'flex',
+      justifyContent: 'end',
+      padding: 2,
+      '& > :not(style) + :not(style)': {
+        ml: 2,
+      },
+    }}
+  >
     <Link to="/">Home</Link>
     <Link to="/form">Form</Link>
     <Link to="/playground">Playground</Link>
-  </header>
+  </Box>
 );
