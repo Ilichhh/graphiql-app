15d0b1cc1fd80bb18ce30c695fc201430c4977f6
ilya <shakurov.ia@gmail.com>
2023-05-06 18:59:09 +0300

    refactor: remove manual variables replacing implementation

--- src/hooks/usePlayground.tsx
+++ src/hooks/usePlayground.tsx
@@ -1,13 +1,5 @@
 import { useCallback, useState } from 'react';
 
-import {
-  DefinitionNode,
-  OperationDefinitionNode,
-  GraphQLError,
-  VariableDefinitionNode,
-} from 'graphql';
-import { parse } from 'graphql/language';
-
 export const usePlayground = (
   initialEndpoint: string,
   initialQuery: string,
@@ -19,25 +11,6 @@ export const usePlayground = (
   const [response, setResponse] = useState('');
 
   const sendRequest = useCallback(() => {
-    // Extract variables definitions from the parsed query
-    let variableDefinitions: VariableDefinitionNode[];
-    try {
-      const parsedQuery = parse(query);
-      variableDefinitions = parsedQuery.definitions
-        .filter(
-          (def: DefinitionNode): def is OperationDefinitionNode =>
-            def.kind === 'OperationDefinition'
-        )
-        .flatMap((def) => def.variableDefinitions || []);
-    } catch (error) {
-      if (error instanceof GraphQLError) {
-        console.log(error);
-        return;
-      }
-      throw error;
-    }
-
-    // Parse the variables string into an object
     let parsedVariables: Record<string, unknown> = {};
     try {
       parsedVariables = variables ? JSON.parse(variables) : {};
@@ -49,25 +22,12 @@ export const usePlayground = (
       throw error;
     }
 
-    // Replace the variable reference in the query with its value
-    let replacedQuery = query;
-    for (const def of variableDefinitions) {
-      const variableName = def.variable.name.value;
-
-      replacedQuery = replacedQuery.replace(/(\(|\s)*\$[^\s]+\s*:\s*[^\s]+\s*(\))?/g, '');
-
-      const regex = new RegExp(`\\$${variableName}(?=[^A-Za-z0-9_]|$)`, 'g');
-      replacedQuery = replacedQuery.replace(regex, JSON.stringify(parsedVariables[variableName]));
-    }
-    console.log(replacedQuery);
-
-    // Send request
     fetch(endpoint, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
       },
-      body: JSON.stringify({ query: replacedQuery }),
+      body: JSON.stringify({ query, variables: parsedVariables }),
     })
       .then((res) => res.json())
       .then((json) => JSON.stringify(json, null, 2))
