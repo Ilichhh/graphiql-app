84721c3a79528a13f6665291d1321b7adc74cded
VaultDweller13 <grognak13@gmail.com>
2023-05-15 00:43:51 +0400

    fix: fix infinite useGraphQLSchema hook call

--- src/hooks/useGraphQLSchema.ts
+++ src/hooks/useGraphQLSchema.ts
@@ -1,44 +1,57 @@
+import { useEffect, useState } from 'react';
 import { buildClientSchema } from 'graphql/utilities';
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 import { useGetSchemaQuery } from '../store/apiSlice';
 import { useAppDispatch } from './reduxTypedHooks';
 import { setError } from '../store/errorSlice';
 
+type Schema = ReturnType<typeof buildClientSchema> | null;
+
 export const useGraphQLSchema = (endpoint: string) => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
   const dispatch = useAppDispatch();
-
-  let systemError = '';
-  let schema = null;
-  let schemaErrorMessage = '';
   const { data, isError, error } = useGetSchemaQuery(endpoint, { skip: !endpoint });
+  const [schema, setSchema] = useState<Schema>(null);
+  const [schemaErrorMessage, setSchemaErrorMessage] = useState('');
 
-  if (data) {
-    try {
-      schema = buildClientSchema(data.data);
-    } catch (e) {
-      if (typeof e === 'string') {
-        systemError = e;
-      } else if (e instanceof Error) {
-        systemError = e.message;
+  useEffect(() => {
+    const buildSchema = () => {
+      let systemError = '';
+
+      if (data) {
+        try {
+          setSchema(buildClientSchema(data.data));
+        } catch (e) {
+          if (typeof e === 'string') {
+            systemError = e;
+          } else if (e instanceof Error) {
+            systemError = e.message;
+          }
+
+          dispatch(setError(systemError));
+        }
       }
 
-      schema = null;
-      dispatch(setError(systemError));
-    }
-  }
-
-  if (error) {
-    if ('status' in error) {
-      if (error.status === 'FETCH_ERROR') {
-        schemaErrorMessage = t(`playground.schemaError`);
-      } else {
-        schemaErrorMessage = 'error' in error ? error.error : JSON.stringify(error.data);
+      if (error) {
+        if ('status' in error) {
+          if (error.status === 'FETCH_ERROR') {
+            setSchemaErrorMessage('playground.schemaError');
+          } else {
+            setSchemaErrorMessage('error' in error ? error.error : JSON.stringify(error.data));
+          }
+        } else {
+          setSchemaErrorMessage(error.message || 'Unknown error');
+        }
       }
-    } else {
-      schemaErrorMessage = error.message || 'Unknown error';
-    }
-  }
+    };
+
+    buildSchema();
+
+    return () => {
+      setSchemaErrorMessage('');
+      setSchema(null);
+    };
+  }, [data, error, dispatch, setSchemaErrorMessage]);
 
   return { schema, isSchemaError: isError, schemaErrorMessage };
 };
