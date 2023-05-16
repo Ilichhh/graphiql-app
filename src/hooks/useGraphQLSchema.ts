7d251d9e99fb6fd0065cefd8e1cc6329b538bb38
Alena Deviatova <aedeviatova@yandex.ru>
2023-05-16 23:17:24 +0300

    fix: infinite rerender when using useGraphQLSchema hook

--- src/hooks/useGraphQLSchema.ts
+++ src/hooks/useGraphQLSchema.ts
@@ -1,18 +1,18 @@
 import { buildClientSchema } from 'graphql/utilities';
 import { useGetSchemaQuery } from '../store/apiSlice';
+import { useEffect, useState } from 'react';
+import { GraphQLSchema } from 'graphql/type/schema';
 
 export const useGraphQLSchema = (endpoint: string) => {
-  let schema = null;
-  let errorMessage = '';
+  const [schema, setSchema] = useState<GraphQLSchema | null>(null);
   const { data, isError, error } = useGetSchemaQuery(endpoint, { skip: !endpoint });
+  const schemaErrorMessage = error?.toString() || '';
 
-  if (data && !isError && data.data) {
-    schema = buildClientSchema(data.data);
-  }
+  useEffect(() => {
+    if (data && !isError && data.data) {
+      setSchema(buildClientSchema(data.data));
+    }
+  }, [data, isError]);
 
-  if (error) {
-    errorMessage = error.toString();
-  }
-
-  return { schema, isSchemaError: isError, schemaErrorMessage: errorMessage };
+  return { schema, isSchemaError: isError, schemaErrorMessage };
 };
