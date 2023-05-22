a6ab09ff587bdf338098cf495a1d2a9cd032ace7
ilya <shakurov.ia@gmail.com>
2023-05-22 21:45:50 +0300

    feat: add dispatch endpoint when create tab from template

--- src/store/thunks.ts
+++ src/store/thunks.ts
@@ -3,15 +3,17 @@ import { ThunkDispatch } from 'redux-thunk';
 import { RootState } from './store';
 import { addTab } from './tabsSlice';
 import { setQuery, setVariables, setHeaders } from './editorSlice';
+import { setEndpoint } from './endpointSlice';
 
 import { DocumentData } from '@firebase/firestore';
 
 export const addNewTab = (data: DocumentData, templateId: string) => {
-  const { name, query, variables, headers } = data;
+  const { name, endpoint, query, variables, headers } = data;
   return (dispatch: ThunkDispatch<RootState, undefined, any>, getState: () => RootState) => {
     dispatch(addTab({ name, instanceOfTemplate: templateId }));
     const state = getState();
     const tabIdx = state.tabs.selectedIdx;
+    dispatch(setEndpoint({ tabIdx, endpoint }));
     dispatch(setQuery({ tabIdx, query }));
     dispatch(setVariables({ tabIdx, variables }));
     dispatch(setHeaders({ tabIdx, headers }));
